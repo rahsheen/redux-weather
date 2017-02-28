aacb4716ff1bcc80f4d3abb70603f3856295fb3b
R. Anthony Porter <rah@sheenonline.biz>
2017-02-28 17:08:49 -0500

    Added axios dependency and openWeather API request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,16 @@
-const API_KEY = '07893796116a3c571e8455921c1faa9b';
+import axios from 'axios';
 
+const API_KEY = '07893796116a3c571e8455921c1faa9b';
+const ROOT_URL =`http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
