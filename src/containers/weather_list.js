566d594b713c33a8bc1e95743e585c3e2eac837b
R. Anthony Porter <rah@sheenonline.biz>
2017-03-01 18:28:44 -0500

    Added reusable Sparklines component for temps

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
     renderWeather(cityData) {
         const name = cityData.city.name;
+        const temps = cityData.list.map(weather => weather.main.temp);
 
         return (
             <tr key={name}>
+                <td>{name}</td>
                 <td>
-                    {name}
+                    <Chart data={temps} color="orange" />
                 </td>
             </tr>
         );
