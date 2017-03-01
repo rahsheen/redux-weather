ee84df802f70d5fc2bf4819616d7971df8ef09a3
R. Anthony Porter <rah@sheenonline.biz>
2017-02-28 20:38:48 -0500

    Fixed typo in reducer preventing actions from triggering
    Fleshed out method to render each city's data in weather list.

--- src/components/app.js
+++ src/components/app.js
@@ -6,7 +6,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar />
         <WeatherList />
       </div>
     );
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,9 +1,10 @@
 import { FETCH_WEATHER } from '../actions/index';
 
 export default function(state = [], action) {
-    switch(action){
+    switch(action.type){
     case FETCH_WEATHER:
         return [ action.payload.data, ...state];
     }
+
     return state;
 }
