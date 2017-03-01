3e50ae39706e2fa7ed20dea69de3b5e5feb7f2b6
R. Anthony Porter <rah@sheenonline.biz>
2017-02-28 19:18:30 -0500

    Hooked  searchbar to Redux dispatch action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '07893796116a3c571e8455921c1faa9b';
-const ROOT_URL =`http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL =`http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
