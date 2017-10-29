621cb2faab746c7ebf9ffc5ad7c64de59267efc4
Rahsheen Porter <raporter@htt.io>
2017-10-29 11:58:27 -0400

    Convert fully to Standard JS

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -2,17 +2,20 @@ import React, { Component } from 'react'
 
 export default class GoogleMap extends Component {
   componentDidMount () {
-    const map = new window.google.maps.Map(this.refs.map, {
-      zoom: 12,
-      center: {
-        lat: this.props.lat,
-        lng: this.props.lon
-      }
-    })
+    const map = new window.google.maps.Map(
+      this.refs.map,
+      {
+        zoom: 12,
+        center: {
+          lat: this.props.lat,
+          lng: this.props.lon
+        }
+      })
+
+    return map
   }
 
   render () {
-    // this.refs.map
     return <div ref='map' />
   }
 }
